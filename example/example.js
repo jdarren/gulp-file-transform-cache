d7371ed34bd8198542981f649fdf18dff84a413a
jdarren <jdarren@gmail.com>
2016-02-15 16:19:49 -0800

    simplification of process for composing gulp plugins with caching mechanism.

--- example/example.js
+++ example/example.js
@@ -3,15 +3,13 @@
 var gulp         = require('gulp'),
     gulpPrefixer = require('./gulpPrefixer'),
     through      = require('through2'),
-    gulpTfc      = require('./lib');
+    gulpTfc      = require('../lib');
 
 gulp.src(['data/*.txt'])
-    .pipe(gulpTfc({path: '.sampleCache',
-              transformPlugins: [
-                  gulpPrefixer.bind(null,'** Fred\n'),
-                  gulpPrefixer.bind(null,'** Joe\n')
-              ]
-          }))
+    .pipe(gulpTfc({ path: '.sampleCache',
+       transformStream: gulpPrefixer('** Fred\n')
+                        .pipe(gulpPrefixer('** Joe\n'))
+    }))
     .pipe((() => {
         return through.obj( (file,enc, next) => {
             console.log(file.path);
